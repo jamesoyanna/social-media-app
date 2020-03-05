a48838af51209ce3a74e343067060d0e84c45591
eshoiza <engrjayt200@gmail.com>
2020-03-05 17:38:45 +0100

    change from firebase timstamp to current date

--- functions/index.js
+++ functions/index.js
@@ -33,7 +33,7 @@ app.post('/scream',(req, res)=>{
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString();
     };
     admin.firestore()
     .collection('screams')
