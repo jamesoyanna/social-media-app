09f0ad7b2b3e96305d3bf9d2ab73696fcdb3446d
eshoiza <engrjayt200@gmail.com>
2020-03-09 16:43:37 +0100

    added a auth middleware

--- functions/index.js
+++ functions/index.js
@@ -43,7 +43,27 @@ app.get('/screams', (req,res)=>{
       .catch(err => console.error(err));
 })
 
-app.post('/scream',(req, res)=>{
+
+const FBAuth = (req, res, next)=>{
+  let idToken;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    idToken = req.headers.authorization.split('Bearer')[1];
+  }else{
+    console.error('No token found')
+    return res.status(403).json({error: 'Unauthorized'})
+  }
+  admin.auth().verifyIdToken(idToken);
+  return db.collection('users')
+  .where('userId', '==', req.user.uid)
+  .limit(1);
+  .get();
+}
+.then(data=>{
+  req.user.handle = data.docs[0].data().handle;
+  return next();
+})
+
+app.post('/scream', FBAuth,(req, res)=>{
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
