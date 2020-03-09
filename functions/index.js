571a5ac029d1650e07edba14c0581ea6234e6c99
eshoiza <engrjayt200@gmail.com>
2020-03-09 13:25:18 +0100

    created an helper function to handle email submission

--- functions/index.js
+++ functions/index.js
@@ -60,6 +60,17 @@ app.post('/scream',(req, res)=>{
         console.error(err);
     })
 })
+
+const isEmail = (email)=>{
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else false
+}
+
+const isEmpty = (string)=>{
+if(string.trim()==='') return true;
+else return false
+}
 // Signup Route
 app.post('/signup', (req, res)=>{
     const newUser = {
